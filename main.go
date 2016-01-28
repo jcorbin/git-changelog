e02b17475838ac0a9ee3193d80db89974c2b9fcc
Joshua T Corbin <jcorbin@wunjo.org>
2016-01-28 11:29:13 -0800

    main: run git-log with since/until refs

--- main.go
+++ main.go
@@ -5,12 +5,39 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 )
 
 var projectGithub = flag.String("github", "", "project github user/repo")
 
 func main() {
 	flag.Parse()
+
+	sinceRef := "ORIG_HEAD"
+	untilRef := "HEAD"
+
+	args := flag.Args()
+
+	if len(args) > 0 {
+		sinceRef, args = args[0], args[1:]
+	}
+
+	if len(args) > 0 {
+		untilRef, args = args[0], args[1:]
+	}
+
+	// TODO: simplify scanning by forcing such things as --no-decorate
+	refspec := strings.Join([]string{sinceRef, untilRef}, "..")
+	cmd := exec.Command("git", "log", "--first-parent", refspec)
+	cmd.Stderr = os.Stderr
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
 
 	scanner := NewLogEntScanner(stdout)
 
@@ -41,5 +68,10 @@ func main() {
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		fmt.Printf("git-log %v\n", err)
+		os.Exit(1)
 	}
 }
